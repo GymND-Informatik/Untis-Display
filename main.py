6a2160462e0ed6f4687a93545292fceb255c2aba
kurciqs <90471896+kurciqs@users.noreply.github.com>
2024-04-22 21:32:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 from bs4 import BeautifulSoup
 from datetime import datetime, date
 
+wpflf = open("wpflf.txt", "r").read().split("\n")
+uu = open("uu.txt", "r").read().split("\n")
+reli = open("reli.txt", "r").read().split("\n")
 
-def is_room(text):
-  pass
+
+def is_class(text):
+  if len(text) <= 1:
+    return False
+  if text[0].isnumeric() and text[1].isalpha():
+    return True
+  else:
+    return False
+
+
+def rearrange_values(value):
+  new_value = []
+  for v in value:
+    li = []
+    for w in v:
+      li.append(w)
+    new_value.append(li)
+
+  n = 0
+  for x in value:
+    time = x[0]
+    length = 1
+    hours = []
+    for i in range(10):
+      if "-" in time:
+        length = int(time[4]) - int(time[0]) + 1
+        if i in range(int(time[0]), int(time[0]) + length):
+          hours.append(i)
+      elif i == int(time):
+        hours.append(i)
+    # no tenth hour
+    hours = [x for x in hours if x != 10]
+    # events that are 0 long and that are too long are discarded (set to no duration)
+    if len(hours) == 0 or len(hours) >= 9:
+      new_value[n][0] = "[]"
+      continue
+    new_value[n][0] = str(hours).strip("[").strip("]")
+    n += 1
+
+  return new_value
 
 
 def parse_html(filename):
@@ -36,22 +77,23 @@ def parse_html(filename):
   tokens = []
 
   for element in soup.find_all():
-    if element.get_text().isspace():
-      continue
     # we are only interested in the table rows, they have all the info
     if element.name == 'tr':
       for child in element.contents:
         text = child.get_text().strip("\n").strip()
         if text == "Text":  #information begins
           mode = True
-          continue
-        if mode:
-          if " " in text and "-" not in text:  # this means that we are reading a new class, since all classes have a space in them but there are hours with a dash so we filter those out
+        elif mode:
+          if is_class(text):  # information about a new class starts flowing in
             if current_class:
-              tokens.append(current_class)
+              tokens.append(
+                  current_class
+              )  # append every time for a new class all the information collected about it
             current_class = [text]
             continue
           current_class.append(text)
+
+  # append one last time
   tokens.append(current_class)
 
   # rearrange the tokens once again into a dictionary
@@ -69,141 +111,137 @@ def parse_html(filename):
         li = []
     new_tokens[token[0]] = el
 
+  # edit the hours into an array instead of the original range e.g. "1-2" -> ["1", "2"]
+  for key, value in new_tokens.items():
+    new_tokens[key] = rearrange_values(value)
+
   return new_tokens, message
 
 
+def format_text(soup, x):
+  div = soup.new_tag('div')
+  x1 = x.split("?")
+  if (len(x1) > 1):
+    s_tag = soup.new_tag('s', attrs={"class": "old"})
+    s_tag.string = x1[0]
+    div.append(s_tag)
+    p_tag = soup.new_tag("p", attrs={"class": "new"})
+    p_tag.string = x1[1]
+    div.append(p_tag)
+  else:
+    p_tag = soup.new_tag("p", attrs={"class": "new"})
+    p_tag.string = x1[0]
+    div.append(p_tag)
+  return div
+
+
 def write_new_html(tokens, message):
+  # error handling
+  if len(tokens) == 0:
+    error_message = "<div><h1>Error Occurred in the parsing. Lmao bad luck.</h1><p>Go talk to Herr Proffesor Kaintz.</p><p>Yours truly, Jacques.</p></div>"
+    soup = BeautifulSoup(error_message, 'html.parser')
+    return soup.prettify()
+
+  # open the html template and find the table
   file = open("template.html", 'r')
   soup = BeautifulSoup(file.read(), 'html.parser')
   table = soup.new_tag("table", id="scroll-table")
 
+  # create the table header that will always display the hours 1 through 9
   thead = soup.new_tag("thead")
   new_row = soup.new_tag('tr', class_='list')
   for i in range(10):
     th_tag = soup.new_tag('th')
-    th_tag.string = str(i) if i != 0 else ' '
+    th_tag.string = str(i) if i != 0 else ''
     new_row.append(th_tag)
-
   thead.append(new_row)
   table.append(thead)
 
+  # enter the daily message up top
   soup.find("p").string = soup.find("p").get_text() + message
 
+  # create the table body
   tbody = soup.new_tag("tbody")
-  for key, value in tokens.items():
-    new_row = soup.new_tag('tr', class_='list')
 
+  # create the special subjects lists
+  wpflf_list = {}
+  uu_list = {}
+  reli_list = {}
+
+  for key, value in tokens.items():
+    # create a new row and add a header to it, the class name
+    new_row = soup.new_tag('tr', class_='list')
     th_tag = soup.new_tag('th')
     th_tag.string = key
     new_row.append(th_tag)
 
-    #extract what hours are targeted
-    new_value = []
-    #copy the array
-    for v in value:
-      li = []
-      for w in v:
-        li.append(w)
-      new_value.append(li)
-
-    n = 0
-    for x in value:
-      time = x[0]
-      length = 1
-      hours = []
-      for i in range(11):
-        if "-" in time:
-          length = int(time[4]) - int(time[0]) + 1
-          if i in range(int(time[0]), int(time[0]) + length):
-            hours.append(i - 1)
-        elif i == int(time):
-          hours.append(i - 1)
-      # no tenth hour
-      hours = [x for x in hours if x != 10]
-      # events that are 0 long and that are too long are discarded
-      if len(hours) == 0 or len(hours) >= 9:
-        new_value[n][0] = "[]"
-        continue
-      new_value[n][0] = str(hours).strip("[").strip("]")
-      n += 1
-
-    # keep track of how many supple hours, will be needed to decide if display at all
+    # keep track of how many suppl hours, will be needed to decide if display at all
     supp_counter = []
-    value = new_value
-    print(value)
+
+    # loop through all hours and write the information into table data cells
     for i in range(9):
-      td_tag = soup.new_tag('td')
-      td_tag.attrs["class"] = "leer"
-      td_tag.string = " "
-      for x in value:
-        if str(i) in x[0]:
-          if x[3] == "---":
-            x2 = x[2].split("?")
-            subject = x2[0]
-            if (len(x2) > 1):
-              subject = x2[1]
-            td_tag.string = x[1] + " " + x[2] + " " + "AUSFALL"
-            td_tag.attrs["class"] = "ausfall"
-          else:
-            td_tag.attrs["class"] = "supplieren"
+      try:
+        # create a new data cell and give it empty text and the "leer" class
+        td_tag = soup.new_tag('td')
+        td_tag.attrs["class"] = "leer"
+        td_tag.string = ""
+
+        for x in value:
+          # check for special subjects
+          subject = x[2].split("?")[0] if len(x[2].split("?")) > 1 else x[2]
+          if subject in wpflf:
+            wpflf_list[key] = value
+            # continue
+          elif subject in uu:
+            uu_list[key] = value
+            # continue
+          elif subject in reli:
+            reli_list[key] = value
+            # continue
+
+          # means the hour is in the array of hours that are suppld
+          if str(i + 1) in x[0]:
             supp_counter.append(i)
 
-            div = soup.new_tag('div')
-            x1 = x[1].split("?")
-            if (len(x1) > 1):
-              p_tag = soup.new_tag("p", attrs={"class": "old"})
-              p_tag.append(soup.new_tag("s"))
-              p_tag.s.string = x1[0]
-              td_tag.append(p_tag)
-              p_tag2 = soup.new_tag("p", attrs={"class": "new"})
-              p_tag2.string = x1[1]
-              div.append(p_tag2)
-            else:
-              p_tag = soup.new_tag("p", attrs={"class": "new"})
-              p_tag.string = x1[0]
-              div.append(p_tag)
-            td_tag.append(div)
+            # means the hour completely drops, give it its own class and handle accordingly
+            if x[3] == "---":
+              div = soup.new_tag('div')
+              for k in range(3):
+                td_tag.attrs["class"] = "ausfall"
+                s_tag = soup.new_tag("s", attrs={"class": "old"})
+                s_tag.string = x[2 - k]
+                div.append(s_tag)
+              td_tag.append(div)
 
-            div = soup.new_tag('div')
-            x2 = x[2].split("?")
-            if (len(x2) > 1):
-              p_tag = soup.new_tag("p", attrs={"class": "old"})
-              p_tag.append(soup.new_tag("s"))
-              p_tag.s.string = x2[0]
-              td_tag.append(p_tag)
-              p_tag2 = soup.new_tag("p", attrs={"class": "new"})
-              p_tag2.string = x2[1]
-              div.append(p_tag2)
+            # otherwise we have suppl, mark with class and append the information
             else:
-              p_tag = soup.new_tag("p", attrs={"class": "new"})
-              p_tag.string = x2[0]
-              div.append(p_tag)
-            td_tag.append(div)
+              td_tag.attrs["class"] = "supplieren"
+              # append new divs to the table data, each for every information, teacher, class, room
+              td_tag.append(format_text(soup, x[1]))
+              td_tag.append(format_text(soup, x[2]))
+              td_tag.append(format_text(soup, x[3]))
 
-            div = soup.new_tag('div')
-            x3 = x[3].split("?")
-            if (len(x3) > 1):
-              p_tag = soup.new_tag("p", attrs={"class": "old"})
-              p_tag.append(soup.new_tag("s"))
-              p_tag.s.string = x3[0]
-              td_tag.append(p_tag)
-              p_tag2 = soup.new_tag("p", attrs={"class": "new"})
-              p_tag2.string = x3[1]
-              div.append(p_tag2)
-            else:
-              p_tag = soup.new_tag("p", attrs={"class": "new"})
-              p_tag.string = x3[0]
-              div.append(p_tag)
-            td_tag.append(div)
-            
+        # append the table data from above to the row
+        new_row.append(td_tag)
 
-      new_row.append(td_tag)
+      except Exception as e:
+        # handle an error by replacing information with "ERROR"
+        td_tag = soup.new_tag('td')
+        td_tag.attrs["class"] = "error"
+        td_tag.string = "ERROR"
+        print("Error handling hour", i, "for class", key, "with error:", e)
+        new_row.append(td_tag)
+
+      # if there aren't any suppl hours at all, or they are too much (like a school trip), skip the class entirely
       if len(supp_counter):
         tbody.append(new_row)
 
-    table.append(tbody)
-
+  # append the whole table body to the table and the table to the div
+  table.append(tbody)
   soup.find("div").append(table)
+
+  print(wpflf_list, uu_list, reli_list)
+
   return soup.prettify()
 
 
@@ -212,20 +250,22 @@ tokens = {}
 message = ""
 # tokens, message = parse_html("subst_002.htm")
 while True:
+  filename = "subst_" + str(num).zfill(3) + ".htm"
+  print("parsing", filename)
   try:
-    filename = "subst_" + str(num).zfill(3) + ".htm"
     tokens_, msg = parse_html(filename)
-    print("parsing", filename)
     if msg != "":
       message = msg
     for a, b in tokens_.items():
       tokens[a] = b
-
     num += 1
-  except FileNotFoundError:
+  except Exception as e:
+    error_message = f"Error Occurred: {str(e)}"
+    print("Error parsing file", filename, "|", error_message)
     break
 
-# print(tokens, message)
+print(tokens, message)
+print(wpflf, uu, reli)
 new_html = write_new_html(tokens, message)
 with open("new.html", "w") as file:
   file.write(new_html)
