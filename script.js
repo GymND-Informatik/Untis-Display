317fda6a9c65905c80618d441ec8a4141b083a95
Filip Jaskovic <75838849+FilipJaskovic@users.noreply.github.com>
2024-04-22 09:38:30 +0200

    commit

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ $(document).ready(function() {
     var originalTableBody = table.find('tbody').first();
     var clonedTableBody = originalTableBody.clone();
 
-    // Append the cloned table body to the table for a seamless loop
     table.append(clonedTableBody);
 
-    var scrollSpeed = 0.5;  // Speed of scrolling, adjust as needed
-    var scrollInterval = 40;  // Interval of scrolling in milliseconds, adjust for smoothness
+    var scrollSpeed = 0.5;
+    var scrollInterval = 40;
 
     setInterval(function () {
         var scrollTop = table.scrollTop();
@@ -16,12 +15,10 @@ $(document).ready(function() {
 
         // Check if the scroll has reached near the end of the cloned table body
         if (scrollTop + visibleHeight >= scrollHeight - 20) {  // 20 is a small buffer to initiate before hitting the very bottom
-            // Clone and append again before reaching the end
             var newClonedBody = originalTableBody.clone();
             table.append(newClonedBody);
         }
 
-        // Smoothly scroll without resetting
         table.scrollTop(scrollTop + scrollSpeed);
 
     }, scrollInterval);
