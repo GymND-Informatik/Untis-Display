3cb95738a0dec899bdcbf3482d313cc706ab80c8
Filip Jaskovic <75838849+FilipJaskovic@users.noreply.github.com>
2024-05-29 14:56:25 +0200

    commit

--- script.js
+++ script.js
@@ -8,22 +8,39 @@ $(document).ready(function() {
 
     var scrollSpeed = 1;
     var scrollInterval = 65;
-
-    setInterval(function () {
-        var scrollTop = table.scrollTop();
-        var scrollHeight = table.prop('scrollHeight');
-        var visibleHeight = table.height();
-
-        // Check if the scroll has reached near the end of the cloned table body
-        if (scrollTop + visibleHeight >= scrollHeight - 20) {
+    //
+    // setInterval(function () {
+    //     var scrollTop = table.scrollTop();
+    //     var scrollHeight = table.prop('scrollHeight');
+    //     var visibleHeight = table.height();
+    //
+    //     // Check if the scroll has reached near the end of the cloned table body
+    //     if (scrollTop + visibleHeight >= scrollHeight - 20) {
             var newClonedBody = originalTableBody.clone();
             var newDivider = divider.clone();
             table.append(newClonedBody);
             table.append(newDivider);
-        }
+    //     }
+    //
+    //     table.animate()
+    //
+    // }, scrollInterval);
+    let counter = 1;
+    function FilipStupid() {
+        table.animate({
+            scrollTop: table.height() * (counter++)
+        }, 30000, 'linear', () => {
+            var newClonedBody = originalTableBody.clone();
+            var newDivider = divider.clone();
+            table.append(newClonedBody);
+            table.append(newDivider);
+            console.log("added new element")
+        });
+        FilipStupid();
 
-        table.scrollTop(scrollTop + scrollSpeed);
+    }
+    FilipStupid();
 
-    }, scrollInterval);
+
 });
 
